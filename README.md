0809c9adb75ca28663099d270623172fa16ff75f
Maria Pogosjan <maria.pogosjan@gmail.com>
2023-06-09 11:17:59 +0200

    new url

--- README.md
+++ README.md
@@ -24,4 +24,4 @@ I had a huge problem with connecting my heart button and the amount of likes, an
 ## View it live
 
 Write your lovely happy thoughts here: https://your-happy-thoughts.netlify.app/
-BE: https://project-happy-thoughts-api-iyve5wmsva-lz.a.run.app/ 
+BE: https://project-happy-thoughts-api-5j5sndevjq-lz.a.run.app/
--- code/src/reusable/urls.js
+++ code/src/reusable/urls.js
@@ -1,2 +1,2 @@
-export const API_URL = (page, perPage) => `https://project-happy-thoughts-api-iyve5wmsva-lz.a.run.app/thoughts?page=${page}&size=${perPage}`
-export const LIKES_URL = (messageID) => `https://project-happy-thoughts-api-iyve5wmsva-lz.a.run.app/thoughts/${messageID}/like`
+export const API_URL = (page, perPage) => `https://project-happy-thoughts-api-5j5sndevjq-lz.a.run.app/thoughts?page=${page}&size=${perPage}`
+export const LIKES_URL = (messageID) => `https://project-happy-thoughts-api-5j5sndevjq-lz.a.run.app/thoughts/${messageID}/like`
